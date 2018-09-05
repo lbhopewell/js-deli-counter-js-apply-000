10446bbcb15fa05cfe00c7620c3f581b0e083f50
Bruce Hopewell <lbhopewell@outlook.com>
2018-09-05 19:09:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(deliLine){
   if(deliLine.length ===0){
     return "The line is currently empty."
   }else{
-    for(let i=0; i<deliLine.length ; ++i){
+    for(let i=0; i<deliLine.length ; i++){
     current += i+1
     current += ". "
     current += deliLine[i];
